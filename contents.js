1530fd973c3c6776dea85286db75a6a1c384a9f3
Oleg Shalaev <oleg@leanws.com>
2018-11-16 17:54:31 -0500

    updated contents.js

--- contents.js
+++ contents.js
@@ -3,11 +3,14 @@
 const allPages={
 'main':[['/','main page'],['plans','plans and prices'],['domains','domain availability'],['custom','customize your order']],
 'about':[['about','about Lean Web Solutions'],['busemail','email'],['time','download time'], ['payments','online payments'], ['appts','scheduling appointments online'], ['design','design'], ['info','info (wiki) service'], ['hosting','hosting']],
-'wiki':[['https://wiki.leanws.com/Appointments','appointments'],['https://wiki.leanws.com/Email','business email'],['https://wiki.leanws.com/Ftp','secure FTP account'],['https://wiki.leanws.com/SEO','search engine optimization (SEO)'],['https://wiki.leanws.com/SPF,_DKIM,_DMARC','SPF, DKIM, DMARC'],['https://wiki.leanws.com/Privacy_Protection','privacy protection'],['https://wiki.leanws.com/Hosting','hosting'],['https://wiki.leanws.com/Optimization','optimization'],['https://wiki.leanws.com/Git','git']],
+'wiki':[['Appointments','appointments'],['Email','business email'],['Ftp','secure FTP account'],['SEO','search engine optimization (SEO)'],['SPF,_DKIM,_DMARC','SPF, DKIM, DMARC'],['Privacy_Protection','privacy protection'],['Hosting','hosting'],['Optimization','optimization'],['Git','git']],
 'other':[['privacy','privacy notice'],['terms','terms and conditions']]};
+const addrPrefixes={'wiki':'https://wiki.leanws.com/'};
 function cOnLoad(){
 loadScript("2.js").then(function(){
-for(var p in allPages) html(p,allPages[p].sort((l,r)=>l[1].toUpperCase()<r[1].toUpperCase()?-1:1).map(x=>'<a class="wiki pad" href="'+x[0]+'">'+x[1]+'</a>').join(""));
+for(var p in allPages){
+var prefix=p in addrPrefixes? addrPrefixes[p]:'';
+html(p,allPages[p].sort((l,r)=>l[1].toUpperCase()<r[1].toUpperCase()?-1:1).map(x=>'<a class="wiki pad" href="'+prefix+x[0]+'">'+x[1]+'</a>').join(""))}
 hideID("preloader")})}
 (function(){
 var ps=[];
